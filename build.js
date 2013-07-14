651ab082ae64bc50c2da5b57c14b022586fe4b9f
cryptoquick <cryptoquick@gmail.com>
2013-07-14 09:37:56 -0600

    Initial commit.

---
+++ build.js
@@ -0,0 +1,60 @@
+// requires
+var fs = require('fs')
+var crypto = require('crypto')
+
+// funcs
+function hash (str) {
+  return crypto.createHash('md5').update(str).digest('hex');
+}
+
+// vars
+var searches = {
+  styles: /~~STYLESHASH~~/,
+  content: /~~CONTENT~~/,
+  bundle: /~~BUNDLEHASH~~/
+}
+
+var files = {
+  html: 'orig.html',
+  styles: 'styles.css',
+  data: 'data.json',
+  js: 'main.js'
+}
+
+// read files
+var strings = {}
+
+for (var f in files) {
+  strings[f] = fs.readFileSync(files[f], 'utf8')
+}
+
+// hash strings
+var hashes = {
+  styles: hash(strings.styles).substr(0, 8),
+  bundle: hash(strings.js).substr(0, 8)
+}
+
+// replace for searches in file
+strings.html = strings.html.replace(searches.styles, hashes.styles)
+strings.html = strings.html.replace(searches.bundle, hashes.bundle)
+
+// remove old files
+var out = 'dist'
+
+if (fs.existsSync(out)) {
+  files = fs.readdirSync(out)
+  files.forEach(function (file) {
+    fs.unlinkSync(out + '/' + file)
+  })
+  fs.rmdirSync(out)
+}
+
+// write new files
+fs.mkdirSync(out)
+
+fs.writeFileSync(out + '/styles.' + hashes.styles + '.css', strings.styles)
+fs.writeFileSync(out + '/bundle.' + hashes.bundle + '.js', strings.js)
+fs.writeFileSync(out + '/index.html', strings.html)
+
+fs.symlink(__dirname + '/favicon.ico', out + '/favicon.ico')
+fs.symlink(__dirname + '/images', out + '/images')
---
+++ readme.md
@@ -0,0 +1,9 @@
+infocraft
+=========
+
+personal website for Hunter Trujillo
+
+controls
+a / d - category nav
+w / s - subcategory nav
+q / e - thumbnail / slide nav
